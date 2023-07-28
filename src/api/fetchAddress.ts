997ef218a3031df8d2b9f081358e1d05d864bae6
matsuhisa <h-matsuhisa@meinan.net>
2023-07-28 10:19:29 +0900

    API アクセスの処理が冗長だったので修正

--- src/api/fetchAddress.ts
+++ src/api/fetchAddress.ts
@@ -2,13 +2,10 @@ import { getApiData } from "./apiHandler";
 import { LatLng } from "leaflet";
 import { Address, AddressResult } from "../type";
 
-export const fetchAddress = async (position: LatLng) => {
-  const load = async (): Promise<Address> => {
-    const data: AddressResult = await getApiData(
-      `https://mreversegeocoder.gsi.go.jp/reverse-geocoder/LonLatToAddress?lat=${position.lat}&lon=${position.lng}`
-    );
-    const address = { muniCd: data.results.muniCd, lv01Nm: data.results.lv01Nm };
-    return address;
-  };
-  return load();
+export const fetchAddress = async (position: LatLng): Promise<Address> => {
+  const data: AddressResult = await getApiData(
+    `https://mreversegeocoder.gsi.go.jp/reverse-geocoder/LonLatToAddress?lat=${position.lat}&lon=${position.lng}`
+  );
+  const address = { muniCd: data.results.muniCd, lv01Nm: data.results.lv01Nm };
+  return address;
 };
